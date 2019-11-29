a91f4b46cf95de035485b21183116e567304dbe6
miyaz <miyazato@sencorp.co.jp>
2019-11-29 20:03:55 +0900

    弾を発射してインベーダーを撃ち落とせるように

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"bufio"
 	"math/rand"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
+	"unicode/utf8"
 
 	"github.com/nsf/termbox-go"
 )
 
 const (
-	_barWidth = 10
-	_height   = 40
-	_width    = 120
+	_height = 40
+	_width  = 120
 )
 
 type point struct {
@@ -25,16 +26,35 @@ var mu sync.Mutex
 
 //ステータス
 type state struct {
-	BarX      int
 	End       bool
+	Player    *player
 	Invaders  map[int]*invader
+	Bullets   map[int]*bullet
 	Life      int
 	Score     int
 	HighScore int
 }
 
+type player struct {
+	Form     string
+	Rows     int
+	Cols     int
+	ColorMap map[string]termbox.Attribute
+	Pos      point
+}
+
 type invader struct {
 	Forms    []string
+	Rows     int
+	Cols     int
+	Color    termbox.Attribute
+	Pos      point
+	Vec      point
+	Interval int
+}
+
+type bullet struct {
+	Form     string
 	Rows     int
 	Cols     int
 	Color    termbox.Attribute
@@ -81,13 +101,16 @@ func drawLoop(sch chan state) {
 			drawLine(0, i, "|")
 			drawLine(_width, i, "|")
 		}
-		drawLine(st.BarX, _height-2, "-========-")
+		drawPlayer(st.Player)
 		if st.End == false {
 			for k, _ := range st.Invaders {
 				drawInvader(*st.Invaders[k])
 			}
+			for k, _ := range st.Bullets {
+				drawBullet(*st.Bullets[k])
+			}
 		} else {
-			drawLine(0, _height/2, "|                                PUSH SPACE KEY")
+			drawLine(0, _height/2, "|                                PUSH ENTER KEY")
 		}
 		termbox.Flush()
 		mu.Unlock()
@@ -99,6 +122,25 @@ func drawLine(x, y int, str string) {
 	runes := []rune(str)
 	for i := 0; i < len(runes); i++ {
 		termbox.SetCell(x+i, y, runes[i], termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
+//プレイヤーを描画
+func drawPlayer(player *player) {
+	scanner := bufio.NewScanner(strings.NewReader(player.Form))
+	j := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes := []rune(line)
+		for i := 0; i < len(runes); i++ {
+			color := termbox.ColorDefault
+			mapkey := strconv.Itoa(i) + "-" + strconv.Itoa(j)
+			if _, ok := player.ColorMap[mapkey]; ok {
+				color = player.ColorMap[mapkey]
+			}
+			termbox.SetCell(player.Pos.X+i, player.Pos.Y+j, runes[i], color, termbox.ColorDefault)
+		}
+		j++
 	}
 }
 
@@ -119,6 +161,14 @@ func drawInvader(invader invader) {
 	}
 }
 
+//弾丸を描画
+func drawBullet(bullet bullet) {
+	runes := []rune(bullet.Form)
+	for i := 0; i < len(runes); i++ {
+		termbox.SetCell(bullet.Pos.X+i, bullet.Pos.Y, runes[i], bullet.Color, termbox.ColorDefault)
+	}
+}
+
 func choice(len int) int {
 	rand.Seed(time.Now().UnixNano())
 	i := rand.Intn(len)
@@ -127,6 +177,8 @@ func choice(len int) int {
 
 //ゲームメイン処理
 func controller(st state, stateCh chan state, keyCh chan termbox.Key, moveCh chan int) {
+	bulletCh := make(chan int)
+	bulletCnt := 0
 	for {
 		select {
 		case key := <-keyCh: //キーイベント
@@ -137,17 +189,24 @@ func controller(st state, stateCh chan state, keyCh chan termbox.Key, moveCh cha
 				mu.Unlock()
 				return
 			case termbox.KeyArrowLeft: //ひだり
-				if st.BarX-3 > 0 {
-					st.BarX -= 3
+				if st.Player.Pos.X-3 > 0 {
+					st.Player.Pos.X -= 3
 				}
 				break
 			case termbox.KeyArrowRight: //みぎ
-				if st.BarX+_barWidth+3 < _width {
-					st.BarX += 3
+				if st.Player.Pos.X+st.Player.Cols+3 < _width {
+					st.Player.Pos.X += 3
 				}
 				break
-			case termbox.KeySpace, termbox.KeyEnter: //ゲームスタート
+			case termbox.KeyEnter: //ゲームスタート
 				st.End = false
+				break
+			case termbox.KeySpace: //発射
+				bulletCnt++
+				st.Bullets[bulletCnt] = fire(st.Player.Pos.X + st.Player.Cols/2)
+				go func(key, ticker int) {
+					moveLoop(bulletCh, key, ticker)
+				}(bulletCnt, st.Bullets[bulletCnt].Interval)
 				break
 			}
 			mu.Unlock()
@@ -165,6 +224,17 @@ func controller(st state, stateCh chan state, keyCh chan termbox.Key, moveCh cha
 			mu.Unlock()
 			stateCh <- st
 			break
+		case i := <-bulletCh: //タイマーイベント
+			mu.Lock()
+			if st.End == false {
+				if _, ok := st.Bullets[i]; ok {
+					st.Bullets[i].Pos.Y += st.Bullets[i].Vec.Y
+					st = checkHit(st, i)
+				}
+			}
+			mu.Unlock()
+			stateCh <- st
+			break
 		}
 	}
 }
@@ -172,11 +242,41 @@ func controller(st state, stateCh chan state, keyCh chan termbox.Key, moveCh cha
 //初期化
 func initGame() state {
 	st := state{End: true}
-	st.BarX = _width/2 - _barWidth/2
 	st.Life = 3
+	st.Player = initPlayer()
 	st.Invaders = initInvaders()
+	st.Bullets = map[int]*bullet{}
 
 	return st
+}
+
+func initPlayer() *player {
+	form := strings.TrimLeft(`
+  ▙▉▟
+▞▓░▒░▓▚`, "\n")
+	colormap := map[string]termbox.Attribute{
+		"2-0": termbox.ColorGreen,
+		"3-0": termbox.ColorBlue,
+		"4-0": termbox.ColorGreen,
+		"0-1": termbox.ColorGreen,
+		"1-1": termbox.ColorCyan,
+		"2-1": termbox.ColorMagenta,
+		"3-1": termbox.ColorRed,
+		"4-1": termbox.ColorMagenta,
+		"5-1": termbox.ColorCyan,
+		"6-1": termbox.ColorGreen,
+	}
+	rows := strings.Count(form, "\n") + 1
+	cols := utf8.RuneCountInString(strings.Split(form, "\n")[rows-1])
+	player := player{
+		Form:     form,
+		Rows:     rows,
+		Cols:     cols,
+		ColorMap: colormap,
+		Pos:      point{X: _width/2 - cols/2, Y: _height - rows},
+	}
+
+	return &player
 }
 
 func initInvaders() map[int]*invader {
@@ -200,7 +300,7 @@ func initInvaders() map[int]*invader {
 	plusminus := []int{-1, 1}
 	invaders := map[int]*invader{}
 	rows := strings.Count(form1, "\n") + 1
-	cols := 6
+	cols := utf8.RuneCountInString(strings.Split(form1, "\n")[rows-1])
 	for i := 0; i < 20; i++ {
 		invaders[i] = &invader{
 			Forms:    []string{form1, form2},
@@ -215,11 +315,23 @@ func initInvaders() map[int]*invader {
 	return invaders
 }
 
+func fire(x int) *bullet {
+	return &bullet{
+		Form:     "▘",
+		Rows:     1,
+		Cols:     1,
+		Color:    termbox.ColorDefault,
+		Pos:      point{X: x, Y: _height - 3},
+		Vec:      point{X: 0, Y: -1},
+		Interval: 50,
+	}
+}
+
 //衝突判定
 func checkCollision(st state, i int) state {
-	//バーが右の壁に到達
-	if st.BarX+_barWidth > _width {
-		st.BarX -= 3
+	//Playerが右の壁に到達
+	if st.Player.Pos.X+st.Player.Cols > _width {
+		st.Player.Pos.X -= 3
 	}
 
 	//左の壁
@@ -294,15 +406,41 @@ func checkCollision(st state, i int) state {
 		}
 	}
 
-	//バーとの衝突判定
-	if st.Invaders[i].Pos.X+st.Invaders[i].Cols >= st.BarX && st.Invaders[i].Pos.X <= st.BarX+_barWidth &&
+	//Playerとの衝突判定
+	if st.Invaders[i].Pos.X+st.Invaders[i].Cols >= st.Player.Pos.X &&
+		st.Invaders[i].Pos.X <= st.Player.Pos.X+st.Player.Cols &&
 		st.Invaders[i].Pos.Y == _height-2-st.Invaders[i].Rows {
-		delete(st.Invaders, i)
+		st.Invaders[i].Vec.Y = -1
+		if st.Invaders[i].Pos.X+(st.Invaders[i].Cols/2) <= st.Player.Pos.X+(st.Player.Cols/2) {
+			st.Invaders[i].Vec.X = -1
+		} else {
+			st.Invaders[i].Vec.X = +1
+		}
+		st.Life--
 	}
 
-	//インベーダー全撃破
-	if len(st.Invaders) == 0 {
-		st.Invaders = initInvaders()
+	return st
+}
+
+func checkHit(st state, i int) state {
+	bullet := st.Bullets[i]
+	//命中判定
+	for o, invader := range st.Invaders {
+		if bullet.Pos.X+bullet.Cols >= invader.Pos.X && bullet.Pos.X <= invader.Pos.X+invader.Cols &&
+			bullet.Pos.Y+bullet.Rows >= invader.Pos.Y && bullet.Pos.Y <= invader.Pos.Y+invader.Rows {
+			delete(st.Bullets, i)
+			delete(st.Invaders, o)
+			//インベーダー全撃破
+			if len(st.Invaders) == 0 {
+				st.Invaders = initInvaders()
+			}
+			return st
+		}
+	}
+
+	//外れたので消す
+	if st.Bullets[i].Pos.Y < 1 {
+		delete(st.Bullets, i)
 	}
 
 	return st
